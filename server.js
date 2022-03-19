85aa47af56842a3f261e4c618c9483d0e17f79a3
Boobalan28 <87655078+Boobalan28@users.noreply.github.com>
2022-03-19 15:13:51 +0530

    Update server.js

--- server.js
+++ server.js
@@ -21,21 +21,6 @@ app.use(cors());
 app.use("/notes", noteRoutes);
 app.use("/users", userRoutes);
 
-// --------------------------deployment------------------------------
-const __dirname = path.resolve();
-
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/build")));
-
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
-  );
-} else {
-  app.get("/", (req, res) => {
-    res.send("API is running..");
-  });
-}
-// --------------------------deployment------------------------------
 
 // Error Handling middlewares
 app.use(notFound);
